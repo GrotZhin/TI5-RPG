5be434365bf78c1ef8b9be0e18859202887b37a5
Túlio Dias <57201326+TTDias@users.noreply.github.com>
2026-09-02 12:13:04 -0300

    Player Root Motion and movement

--- Procedural Map Generation/Assets/Scripts/Player/PlayerMove.cs
+++ Procedural Map Generation/Assets/Scripts/Player/PlayerMove.cs
@@ -11,7 +11,7 @@ public class PlayerMove : MonoBehaviour
 
     Vector3 moveInput = Vector3.zero;
     Vector3 moveDir;
-    float ySpeed, rotationSpeed = 6;
+    float ySpeed, rotationSpeed = 10;
 
     void Start()
     {
@@ -25,6 +25,11 @@ public class PlayerMove : MonoBehaviour
         Vector2 input = context.ReadValue<Vector2>();
         moveInput.x = input.x;
         moveInput.z = input.y;
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        animator.SetBool("IsRunning", context.performed);
     }
 
     public void OnLockTarget(InputAction.CallbackContext context)
@@ -42,7 +47,7 @@ public class PlayerMove : MonoBehaviour
         moveDir = Quaternion.AngleAxis(Camera.main.transform.rotation.eulerAngles.y, Vector3.up) * moveInput;
         moveDir.Normalize();
 
-        //ySpeed += Physics.gravity.y * deltaTime;
+        ySpeed += Physics.gravity.y * deltaTime;
 
         if (moveDir != Vector3.zero)
         {
@@ -51,16 +56,18 @@ public class PlayerMove : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(moveDir, Vector3.up);
             
             float angle = Vector3.SignedAngle(transform.forward, moveDir, Vector3.up);
-            if (Mathf.Abs(angle) < 45)
-            {
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
-                animator.SetFloat("Turn Magnitude", 0);
-            }
-            else
-            {
-                angle = angle/140;
-                animator.SetFloat("Turn Magnitude", angle, 0.035f, deltaTime);
-            }
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
+            //if (Mathf.Abs(angle) < 45)
+            //{
+            //    transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
+            //    animator.SetFloat("Turn Magnitude", 0);
+            //}
+            //else
+            //{
+            //    angle = angle/140;
+            //    animator.SetFloat("Turn Magnitude", angle, 0.035f, deltaTime);
+            //}
         }
         else
         {
@@ -79,7 +86,7 @@ public class PlayerMove : MonoBehaviour
         moveDir = Quaternion.AngleAxis(cc.transform.rotation.eulerAngles.y, Vector3.up) * moveDir;
         moveDir.Normalize();
 
-        //ySpeed += Physics.gravity.y * deltaTime;
+        ySpeed += Physics.gravity.y * deltaTime;
 
         if (moveDir != Vector3.zero)
         {
@@ -97,17 +104,12 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         Move(Time.fixedDeltaTime);
-
-        //Vector3 velocity = moveDir * 4 * Time.fixedDeltaTime;
-        //velocity.y = ySpeed * Time.fixedDeltaTime;
-
-        //cc.Move(velocity);
     }
 
     private void OnAnimatorMove()
     {
         Vector3 velocity = animator.deltaPosition;
-        //velocity.y = ySpeed * Time.deltaTime;
+        velocity.y = ySpeed * Time.deltaTime;
 
         cc.Move(velocity);
         transform.rotation *= animator.deltaRotation;
